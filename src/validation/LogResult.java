37f15e6586c5acbb04013f28833be069b19f7899
Souly <souly@ut.ee>
2015-10-29 20:47:30 +0200

    added fields, do we also need tokens?

--- src/validation/LogResult.java
+++ src/validation/LogResult.java
@@ -4,4 +4,10 @@ package validation;
  * Created by Hans on 29/10/2015.
  */
 public class LogResult {
+
+  private int missingTokens;
+  private int remainingTokens;
+  private int consumedTokens;
+  private int producedTokens;
+
 }
